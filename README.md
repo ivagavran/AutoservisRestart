d4735476abc3884a5d3d786814033bc38c74aa3c
ivagavran <ivagavran14@gmail.com>
2022-05-13 15:44:55 +0200

    LV4 update

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 
 Autoservis Restart projekt je izrade web aplikacije iz kolegija Web programiranje. 
 
-Projekt će biti podijeljen na 5 laboratorijskih vježbi. 
-Svaka vježba imat će svoju granu na kojoj će biti prikazan trenutni napredak kako bi se razvoj aplikacije mogao kontinuirano pratiti.
+Projekt će biti podijeljen na 5 laboratorijskih vježbi. Svaka vježba imat će svoju granu na kojoj će biti prikazan trenutni napredak kako bi se razvoj aplikacije mogao kontinuirano pratiti.
 
 ## Jezici i tehnologije 
 
   1. HTML
   2. CSS
   3. Bootstrap, JavaScript
-  4. 
-  5. 
+  4. JavaScript, jQuery, OwlCarousel2, dopuna sadržaja
+  5. php, formatiranje koda
--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-/*document.querySelector("h1").innerHTML = "Good Bye";*/
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
 
-function mOver(obj) {
-    obj.innerHTML = "Kvalitetni autoservis s 5 zvjezdica!"
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+const modaltime = document.querySelector('#modaltime');
+const openmodaltime = document.querySelector('.open-button2');
+const closemodaltime = document.querySelector('.close-button2');
+
+openmodaltime.addEventListener('click', () => {
+    modaltime.showModal();
+})
+
+closemodaltime.addEventListener('click', () => {
+    modaltime.close();
+})
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-function mOut(obj) {
-    obj.innerHTML = "Održavanje i popravak motornih vozila"
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
-function mDown(obj) {
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.big-heading');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    obj.innerHTML = "245-321";
-}
+anime.timeline({loop: false})
+  .add({
+    targets: '.big-heading .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 100 * i
+  }).add({
+    targets: '.big-heading .letter', 
+    opacity: 1,
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
 
-function mUp(obj) {
+  
 
-    obj.innerHTML = "Kontakt";
-}
